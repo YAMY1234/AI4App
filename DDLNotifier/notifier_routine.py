556e26022a89318116eb03dff3a2c7d5853818f5
YAMY <2682000734@qq.com>
2024-01-04 21:49:13 -0500

    Revise system path position

--- DDLNotifier/notifier_routine.py
+++ DDLNotifier/notifier_routine.py
@@ -1,14 +1,14 @@
 import os
 
-from DDLNotifier.CUHK import ddl_notifier as CUHK
-from DDLNotifier.HKU import ddl_notifier as HKU
-from DDLNotifier.HKUST import ddl_notifier as HKUST
-
 import sys
 # Get the current directory
 current_directory = os.path.dirname(os.path.abspath(__file__))
 # Append the current directory to sys.path
 sys.path.append(current_directory)
+
+from DDLNotifier.CUHK import ddl_notifier as CUHK
+from DDLNotifier.HKU import ddl_notifier as HKU
+from DDLNotifier.HKUST import ddl_notifier as HKUST
 
 
 def main():
