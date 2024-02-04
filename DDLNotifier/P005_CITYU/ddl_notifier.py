fd50a8a872a563bfe605eaed5e6432a7a86858d7
YAMY <2682000734@qq.com>
2024-02-03 21:31:43 -0500

    cityU notifier - update

--- DDLNotifier/P005_CITYU/ddl_notifier.py
+++ DDLNotifier/P005_CITYU/ddl_notifier.py
@@ -1,3 +1,5 @@
+import random
+import time
 from datetime import datetime
 
 from bs4 import BeautifulSoup
@@ -16,11 +18,42 @@ school_name = BASE_PATH.split('_')[-1]
 
 
 def download_html(url):
+    session = requests.Session()
+
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
+        'authority': 'masters.smu.edu.sg',
+        # 'method': 'GET',
+        # 'path': '/programmes',
+        'scheme': 'https',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Cookie': "visid_incap_2480916=8lCch3j7SNu4VgB+SKTQiEuUoGUAAAAAQUIPAAAAAACcl+XTIFHXXamSIk6cel8B; _gcl_au=1.1.330651366.1705022542; _fbp=fb.2.1705022542064.1143887163; __qca=P0-878428341-1705022541950; marker_id_642aa66c7b103f77417a7647=6129d65c-bc37-410d-a35f-7f3eac7f501a; OptanonAlertBoxClosed=2024-01-12T01:22:28.914Z; visid_incap_823419=8nU0TZVtS6CdweSli+6AMLmJsGUAAAAAQUIPAAAAAACEq0qQyy8bbM7lYbUft/L1; _hjSessionUser_2727825=eyJpZCI6IjRmZGVmNWY2LWIwNDgtNTNkMC1iMWNlLWUwZjQ0NjIxOTFkYyIsImNyZWF0ZWQiOjE3MDYwNjg0MTE2NTUsImV4aXN0aW5nIjp0cnVlfQ==; visid_incap_2480929=FQFME0/DR3iZcE3pce67FbuJsGUAAAAAQUIPAAAAAADhDXqzoBs+WjxoviqW0bgR; visid_incap_2480912=lbb3LWCVQeqiIo31bbFVtbuJsGUAAAAAQUIPAAAAAAAIM0plISft/uclR5lk7dBs; visid_incap_2045174=auUMrrYYTVS8Xg0NSLi2MLuJsGUAAAAAQUIPAAAAAACtEdiKC5DtBa2BEvQuIm90; _ga_WGRFPYWXN0=GS1.1.1706068411.1.1.1706068433.38.0.0; incap_ses_1700_2480916=B6XEWGzGsh7S2SMamJ2XFw4Es2UAAAAAbd4ShGdlhsSlU8xWI9wWuA==; _gid=GA1.3.1759554424.1706230801; _clck=1ebtmuv%7C2%7Cfiq%7C0%7C1472; _ga_4Y66YGQ0FG=GS1.1.1706230801.3.1.1706231793.31.0.0; _ga_SKE4KV2FV9=GS1.1.1706230801.3.1.1706231793.31.0.0; _ga=GA1.1.51990194.1705022542; _ga_N1G7RZ7VSB=GS1.3.1706230801.3.1.1706231793.32.0.0; cto_bundle=ythz7l9VWjR0dCUyRnRycWh3Y1dsNnlLSUgyVFdUWXI5N3N5TGlSbk4wQ090eGFhJTJGblg1UVhYNTRoOFcybmUlMkY3QU5OJTJCc2dnbVRQczJRUTF4a2pRRmFWVnF3MjAyNVQwMnolMkI2TjlNTCUyQmNqJTJGNkwxRXE0YkM3QXYlMkZ5VEhzekFWczRjdW1GbHdQVk9zeEtrZ2VxbyUyQnBHRzdLSDg5UG9MdG9GRHZiUDJZTDRoOGtBVHZEUk5RdnN5ZWVBbnl3dWo1ZTJ1MCUyRkZUajl3WXoyY2VYdjJLaTF5TDc3ZXFTblElM0QlM0Q; _clsk=li6gc5%7C1706231793646%7C7%7C1%7Ce.clarity.ms%2Fcollect; OptanonConsent=isIABGlobal=false&datestamp=Thu+Jan+25+2024+20%3A16%3A35+GMT-0500+(Eastern+Standard+Time)&version=6.17.0&hosts=&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0002%3A1%2CC0004%3A1&geolocation=US%3BPA&AwaitingReconsent=false",
+        'Sec-Ch-Ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"macOS"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    }
+
+    # 随机延迟
+    time.sleep(random.uniform(1, 5))
+
+    response = session.get(url, headers=headers)
     response.raise_for_status()
     return response.text
+
+# def download_html(url):
+#     headers = {
+#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+#     response = requests.get(url, headers=headers)
+#     response.raise_for_status()
+#     return response.text
 
 
 def parse_html(html):
