c6b3bb760d4b0223a1f08fff22f1164529a731af
YAMY <2682000734@qq.com>
2024-04-14 21:21:58 -0400

    feat: timestamp-version control

--- DDLNotifier/P021_UCL/program_url_crawler.py
+++ DDLNotifier/P021_UCL/program_url_crawler.py
@@ -36,7 +36,16 @@ def crawl(url="https://www.ucl.ac.uk/prospective-students/graduate/taught-degree
     # 将数据保存到Excel文件
     df.to_excel("programs.xlsx", index=False)
 
+    # 保存    programs-当前时间戳.xlsx
+    df.to_excel(f"programs-{pd.Timestamp.now().strftime('%Y%m%d%H%M%S')}.xlsx", index=False)
 
+    # 检查当前文件夹下时间戳后缀为3天之前的文件，如果存在则删除
+    for file in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+        print(file)
+        if file.startswith("programs-") and file.endswith(".xlsx"):
+            file_time = pd.Timestamp(file.split("-")[1].split(".")[0])
+            if pd.Timestamp.now() - file_time >= pd.Timedelta(days=3):
+                os.remove(file)
 
 # 调用爬虫函数
 if __name__ == '__main__':
