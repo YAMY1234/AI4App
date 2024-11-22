e29c49cc987f66126c0fe2c6e479570e5e187a89
YAMY <2682000734@qq.com>
2024-11-21 21:06:06 -0800

    fix backend ui change for P027_Warwick program_url_crawler

--- DDLNotifier/P027_Warwick/program_url_crawler.py
+++ DDLNotifier/P027_Warwick/program_url_crawler.py
@@ -9,19 +9,29 @@ def crawl(url="https://warwick.ac.uk/study/postgraduate/courses/"):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # The base URL should end with a slash to ensure urljoin appends the relative path correctly.
-    base_url = "https://warwick.ac.uk/study/postgraduate/courses/"
+    # base_url = "https://warwick.ac.uk/study/postgraduate/courses/"
 
-    course_links = soup.select('div.sb-glossary-terms a')
+    # 获取所有的 <a> 标签
+    all_links = soup.find_all('a')
+
+    # 用列表存储符合条件的链接
+    course_links = []
+
+    # 遍历所有的 <a> 标签
+    for link in all_links:
+        href = link.get('href', '')  # 获取 href 属性，如果不存在则返回空字符串
+        if href.startswith("https://warwick.ac.uk/study/postgraduate/courses/"):
+            course_links.append(link)
     data = {'ProgramName': [], 'URL Link': []}
 
     for link in course_links:
         if 'href' in link.attrs:  # Check if the 'href' attribute exists
             program_name = link.get_text(strip=True)
             program_link = link['href']
-            full_url = urljoin(base_url, program_link)  # Properly form the full URL
+            # full_url = urljoin(base_url, program_link)  # Properly form the full URL
 
             data['ProgramName'].append(program_name)
-            data['URL Link'].append(full_url)
+            data['URL Link'].append(program_link)
 
     if data['ProgramName']:
         PROGRAM_DATA_EXCEL = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'programs.xlsx')
